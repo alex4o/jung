51ce6720e058871bccd302e16d691d5f605bae83
alex4o <mrabonin@gmail.com>
2020-03-28 00:11:00 +0200

    Adds router

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,66 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import App from "./components/App";
+import Tasks from "./components/Tasks";
 import { BrowserRouter } from "react-router-dom";
 
 import "./styles/app.less"
 
-ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-    ,
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+} from "react-router-dom";
 
-    document.getElementById("container")
-);
+
+function Home() {
+	return <h2>Home</h2>;
+}
+
+function About() {
+	return <h2>About</h2>;
+}
+
+function Users() {
+	return <h2>Users</h2>;
+}
+
+export default function Index() {
+	return (
+		<Router>
+			<div>
+				<nav>
+					<ul>
+
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/profile">Profile</Link>
+						</li>
+						<li>
+							<Link to="/tasks">Tasks</Link>
+						</li>
+						<li>
+							<Link to="/achivements">Achivements</Link>
+						</li>
+
+
+					</ul>
+				</nav>
+
+				<Switch>
+					<Route path="/profile" component={About} />
+					<Route path="/tasks" component={Tasks} />
+					<Route path="/achivements" component={About} />
+					<Route path="/" component={App} />
+				</Switch>
+			</div>
+		</Router>
+	);
+}
+
+
+
+ReactDOM.render(<Index />, document.getElementById("container"));
