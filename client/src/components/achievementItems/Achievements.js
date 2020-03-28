4ff354eeb30a973be89392072637921692232bde
Nikolay Pashov <nikifaets111@gmail.com>
2020-03-28 02:57:41 +0200

    Adding grids to achievement page

--- client/src/components/achievementItems/Achievements.js
+++ client/src/components/achievementItems/Achievements.js
@@ -1,8 +1,63 @@
 import React from "react";
+import {Col, Row, Divider} from "antd";
 
 export default function Achievements() {
     
 
-    const element = <h1>Hello, world</h1>;
-	ReactDOM.render(element, document.getElementById('root')); 
+	const element = <h1>Hello, world</h1>;
+	var element1 = What();
+	var element2 = createDivider(1, element1);
+	return element2;
+}
+
+function What() {
+
+	return (
+
+		<>
+		<Row span={20}>
+
+			<h2> What </h2>
+
+			<Col offset={3}>
+
+				<h1> Col inside Row </h1>
+			</Col>
+			<Col offset={0}>
+
+				<h1> Col2 inside Row1 </h1>
+
+				<Row>
+
+					<h2> Row inside Column</h2>
+				</Row>
+			</Col>
+			<Col span={50}>
+
+				<h1> Col inside Row </h1>
+			</Col>
+		</Row>
+
+		<Row>
+
+			<h1> Whaaaaat </h1>
+		</Row>
+
+		<Col span={110}>
+
+			<h1> Column </h1>
+		</Col>
+		</>
+	);
+}
+
+function createDivider(num, Components){
+
+	return(
+
+		<Divider>
+			{Components}
+		</Divider>
+	);
+
 }
