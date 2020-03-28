071ba3112fcb6f3ec0f96d0176181a056b41e59a
VikVelev <viktorvelev8@gmail.com>
2020-03-28 02:58:25 +0200

    Perfect Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -23,59 +23,42 @@ export default function Navbar() {
 	};
 	
 	return (
-		<Row style={{ borderBottom: "1px solid #f0f0f0", background: "#001529" }}>
-			<Col span={12}>
-				<Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal" theme={theme}>
-					<Menu.Item key="app">
-						<HomeOutlined />
-						<Link to="/">Home</Link>
-					</Menu.Item>
+		<Menu style={{ marginLeft: '10%', marginRight: '10%'}}
+			onClick={handleClick}
+			selectedKeys={[current]} 
+			mode="horizontal" 
+			theme={theme}>
+			<Menu.Item key="app">
+				<HomeOutlined />
+				<Link to="/">Home</Link>
+			</Menu.Item>
 
-					<Menu.Item key="tasks">
-						<SettingOutlined />
-						<Link to="/tasks">Tasks</Link>
-					</Menu.Item>
+			<Menu.Item key="tasks">
+				<SettingOutlined />
+				<Link to="/tasks">Tasks</Link>
+			</Menu.Item>
 
-					{/* <SubMenu title = {
-							<span className="submenu-title-wrapper">
-								<SettingOutlined />
-								<Link to="/tasks">Tasks</Link>
-							</span>
-						}>
-						<Menu.ItemGroup title="Item 1">
-							<Menu.Item key="setting:1">Test 1</Menu.Item>
-							<Menu.Item key="setting:2">Option 2</Menu.Item>
-						</Menu.ItemGroup>
-						<Menu.ItemGroup title="Item 2">
-							<Menu.Item key="setting:3">Option 3</Menu.Item>
-							<Menu.Item key="setting:4">Option 4</Menu.Item>
-						</Menu.ItemGroup>
-					</SubMenu> */}
+			<Menu.Item key="alipay">
+				<AppstoreOutlined/>
+				<Link to="/achivements">Achivements</Link>
+			</Menu.Item>
 
-					<Menu.Item key="alipay">
-						<AppstoreOutlined/>
-						<Link to="/achivements">Achivements</Link>
-					</Menu.Item>
+			<Menu.Item key="mail">
+				<UserOutlined />
+				<Link to="/profile">Profile</Link>
+			</Menu.Item>
 
-					<Menu.Item key="mail">
-						<UserOutlined />
-						<Link to="/profile">Profile</Link>
-					</Menu.Item>
-				</Menu>
-			</Col>
-			<Col span={6} push={6}>
-				<Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal" theme={theme}>
-					<Menu.Item key="signup">
-						<MailOutlined />
-						<Link to="#">Sign-up</Link>
-					</Menu.Item>
+			<Menu style={{ float: 'right' }} theme={theme}>
+				<Menu.Item key="signup">
+					<MailOutlined />
+					<Link to="#">Sign-up</Link>
+				</Menu.Item>
 
-					<Menu.Item key="login">
-						<LoginOutlined/>
-						<Link to="#">Log in.</Link>
-					</Menu.Item>
-				</Menu>
-			</Col>
-		</Row>
+				<Menu.Item key="login">
+					<LoginOutlined/>
+					<Link to="#">Log in.</Link>
+				</Menu.Item>
+			</Menu>
+		</Menu>
 	)
 }
