dec6e7a23454b3fcd3e677d655d1c2b4fdd379bc
VikVelev <viktorvelev8@gmail.com>
2020-03-28 12:26:42 +0200

    Abstracted Task.js

--- client/src/components/Tasks.js
+++ client/src/components/Tasks.js
@@ -10,50 +10,16 @@ import {
 } from "react-router-dom";
 
 import { usePromise } from "../utils"
+import { Task, IconText } from './task/Task'
 
 import { tasks } from "../stores/db"
-import { Row, Tag, Button, Col, Card, List, PageHeader, Empty } from 'antd';
+import { Row, Tag, Button, Col, Card, List, PageHeader, Empty, Icon } from 'antd';
 import { MessageOutlined, LikeOutlined, StarOutlined } from '@ant-design/icons';
 
 import { Loadable } from "../utils"
 
-const IconText = ({ icon, text }) => (
-	<span>
-		{React.createElement(icon, { style: { marginRight: 8 } })}
-		{text}
-	</span>
-);
-
-
-function Task() {
-	let { id } = useParams();
-	let document = usePromise(tasks.get(id))
-
-	return <Loadable loading={document.loading}
-		loaded={() =>
-			<PageHeader
-				title={document.value.title}
-				className="site-page-header"
-				subTitle={document.value.description}
-				extra={[
-					<Button key="participate">Participate</Button>
-				]}
-				tags={<Tag color="blue">Running</Tag>} >
-				<Row>
-					{document.value._attachments ? <img
-						style={{ objectFit: "cover", flex: 1, width: "100%" }}
-						alt="logo"
-						src={`http://fortress88.servebeer.com:5984/tasks/${id}/${Object.keys(document.value._attachments)[0]}`}
-					/> : <></>}
-				</Row>
-				
-			</PageHeader>
-		} />
-
-
-}
-
 function TaskList() {
+
 	let docs = usePromise(tasks.allDocs({ include_docs: true }))
 	let history = useHistory();
 
