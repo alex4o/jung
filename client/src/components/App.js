765297e720c56f03145a6fa1bad6d2eb0d8ab298
alex4o <mrabonin@gmail.com>
2020-03-28 15:44:37 +0200

    fixed app screen

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,15 +2,13 @@ import React, { Component } from "react";
 import { DatePicker, Button } from 'antd';
 import { useObserver, useLocalStore } from "mobx-react";
 
-import { db } from "../stores/db"
+
 import { usePromise } from "../utils";
 
 export default function App() {
-    let state = usePromise(db.get("0389dc2c1840a65e74d1ec1cf9006bfe"))
 
     return (
         <div>
-            {state.loading ? "Loading ..." : JSON.stringify(state.value)}
             <DatePicker />
             <Button type="primary" style={{ marginLeft: 8 }}>
                 Primary Button
