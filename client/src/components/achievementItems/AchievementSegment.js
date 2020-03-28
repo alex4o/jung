00f9bf57f9e150e810ab1b7551253b553b2b0141
alex4o <mrabonin@gmail.com>
2020-03-28 15:39:49 +0200

    Fixed the logind db usage

--- client/src/components/achievementItems/AchievementSegment.js
+++ client/src/components/achievementItems/AchievementSegment.js
@@ -2,101 +2,35 @@ import React, { useState } from "react";
 import { Row, Col, Divider } from "antd";
 import AchievementCard from './AchievementCard'
 
+import { achievements } from "../../stores/db"
+import { Loadable, usePromise } from "../../utils"
+
 function achievementList(list) {
 	console.log(list)
 	return (
 		<div style={{ display: "flex", flexWrap: "wrap" }}>
-			{list.map((el, i) =>  <AchievementCard key={i} data={el}/>)}
+			{list.map((el, i) => <AchievementCard key={i} data={el} />)}
 		</div>
 	);
 
 }
 
 export default function AchievementSegment() {
-	
-	/**
-	 * Achievement JSON: 
-	 * 
-	 */
-	let mockData = [
-		{
-			"icon": "fly",
-			"type":"",
-			"completed": true,
-			"description": "Use 20 different toilets.",
-			"title": "Amusement"
-		}, {
-			"icon": "car",
-			"type":"",
-			"completed": false,
-			"description": "Create a rocket ship and fly to space.",
-			"title": "Sophistication"
-		}, {
-			"icon": "video",
-			"type":"",
-			"completed": false,
-			"description": "Create a rocket ship and fly to space.",
-			"title": "Sophistication"
-		}, {
-			"icon": "car",
-			"type":"",
-			"completed": false,
-			"description": "Create a rocket ship and fly to space.",
-			"title": "Sophistication"
-		}, {
-			"icon": "fly",
-			"type":"",
-			"completed": false,
-			"description": "Create a rocket ship and fly to space.",
-			"title": "Sophistication"
-		}, {
-			"icon": "video",
-			"type":"",
-			"completed": false,
-			"description": "Create a rocket ship and fly to space.",
-			"title": "Sophistication"
-		}, {
-			"icon": "fly",
-			"type":"",
-			"completed": false,
-			"description": "Create a rocket ship and fly to space.",
-			"title": "Sophistication"
-		}, {
-			"icon": "car",
-			"type":"",
-			"completed": false,
-			"description": "Create a rocket ship and fly to space.",
-			"title": "Sophistication"
-		}, {
-			"icon": "video",
-			"type":"",
-			"completed": false,
-			"description": "Create a rocket ship and fly to space.",
-			"title": "Sophistication"
-		}, {
-			"icon": "car",
-			"type":"",
-			"completed": false,
-			"description": "Create a rocket ship and fly to space.",
-			"title": "Sophistication"
-		}
-	]
 
-	const [ list, setList ] = useState(mockData)
-
-
-	return <div>
+	let { loading, value } = usePromise(achievements.allDocs({ include_docs: true }).then(result => result.rows.map(row => row.doc)));
+	console.log(value)
+	return <Loadable loading={loading} loaded={() => <>
 		<Row gutter={[16, 32]}>
 			<Col span={12} />
 		</Row>
 
 		<Row gutter={[16, 32]}>
-			<Col span={12}/>
-			
-			<Col span={12} style={{borderLeft: '1px #DDD solid'}}> 
-				{achievementList(list)}
+			<Col span={12} />
+
+			<Col span={12} style={{ borderLeft: '1px #DDD solid' }}>
+				{achievementList(loading ? [] : value)}
 			</Col>
 		</Row>
-	</div>
-		
+	</>
+	} />
 }
